667e52d0c07b1ad735df0c6d039a1ff664778b49
chuwu1337 <1997tf2014@mail.ru>
2026-02-21 15:09:55 +0300

    day 1 and day 2

--- week_intensiv/day2_encapsulation/tasks/valid_user.py
+++ week_intensiv/day2_encapsulation/tasks/valid_user.py
@@ -4,4 +4,13 @@ class ValidUser:
     @property
     def password(self): return "********"
     @password.setter
-    def password(self, val): pass
+    def password(self, val):
+        if len(self._password) < 8:
+            raise ValueError('Ваш пароль слишком короткий')
+        a = 0
+        for i in '0123456789':
+            if i in self._password:
+                a = 1
+            if a == 0:
+                raise ValueError('Цифры отсутствуют')
+            self._password = val
